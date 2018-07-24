1894ca74d6ef0d6d73bd871d1b29b681de931bb3
sburns18 <sburns18@holynames-sea.org>
2018-07-24 16:50:02 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -6,12 +6,4 @@ end
 def base_footwork 
 puts "Lift right foot"
 puts "Return right foot to the ground"
-puts" 
-
-
-
-  puts "Base Footwork: "
-  puts "Step three: Lift right foot, return right foot to the ground, finishing with a small skip-step backward"
-  puts "Skip Step Left:Lower left foot to the ground, Bounce left foot back up slightly, kicking it a few inches back, Left, Right, Left and Left."
-  puts "Step five: "
-  puts "Step six: "
+puts "Finishing with a small skip-step backward"
