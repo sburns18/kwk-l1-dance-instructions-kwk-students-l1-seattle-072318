07dca1799c8b52439daf6a0225a0f83e2d03316b
sburns18 <sburns18@holynames-sea.org>
2018-07-24 16:32:46 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,7 @@
-
-# Code your methods below
+def gangnam_style
+  puts "Assume the proper stance: Plant lefs far apart, bend knees slightly and keep posture loose"
+  puts "Base Footwork: "
+  puts "Step three: Lift right foot, return right foot to the ground, finishing with a small skip-step backward"
+  puts "Skip Step Left:Lower left foot to the ground, Bounce left foot back up slightly, kicking it a few inches back, Left, Right, Left and Left."
+  puts "Step five: "
+  puts "Step six: "
